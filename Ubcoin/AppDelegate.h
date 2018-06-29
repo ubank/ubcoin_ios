8dd43451a68478bb8e32e54b9c42046432c2b984
Alex Ostroushko <osapav@gmail.com>
2018-06-29 18:53:50 +0300

    Initial Commit

---
+++ Ubcoin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ubcoin
+//
+//  Created by Alex Ostroushko on 29.06.2018.
+//  Copyright © 2018 UBANK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Ubcoin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ubcoin
+//
+//  Created by Alex Ostroushko on 29.06.2018.
+//  Copyright © 2018 UBANK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
