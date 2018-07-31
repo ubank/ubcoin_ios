24d381a60040a80c45f17bd056c56ebdb2f759f6
Aidar <akhakimzyanov@gmail.com>
2018-07-31 21:06:42 +0300

    update text field

--- Ubcoin/Class/Controls/TextField/UBFloatingPlaceholderTextField.h
+++ Ubcoin/Class/Controls/TextField/UBFloatingPlaceholderTextField.h
@@ -6,28 +6,6 @@
 //  Copyright (c) 2015 uBank. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-#import "UBTextField.h"
-
 @interface UBFloatingPlaceholderTextField : UBTextField
-
-@property (nonatomic, assign) CGFloat spaceBetweenFloatingAndHint;
-@property (nonatomic, assign) CGFloat spaceBetweenFloatingAndText;
-
-@property (nonatomic, strong) UIFont   *headerFont;
-@property (nonatomic, strong) UIFont   *placeholderFont;
-@property (nonatomic, strong) UIColor  *headerTextColor;
-
-@property (nonatomic, strong) NSString *hint;
-
-@property (nonatomic, strong) NSString *errorText;
-@property (nonatomic, strong) UIColor  *errorTextColor;
-
-@property (nonatomic, strong) UIColor *normalTextColor;
-
-@property (nonatomic, assign) BOOL editable;
-
-- (void) showError;
-- (void) hideError;
 
 @end
