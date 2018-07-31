151d1fc8dcc4d7b634f0dce550b987292df11521
Aidar <akhakimzyanov@gmail.com>
2018-07-31 12:13:00 +0300

    text field ready)))

--- Ubcoin/Class/Controls/TextField/UBCTextField.swift
+++ Ubcoin/Class/Controls/TextField/UBCTextField.swift
@@ -7,24 +7,23 @@
 //
 
 import UIKit
-
 import UnderLineTextField
+
 class UBCTextField: UnderLineTextField {
+    
+    private var firstInit = true
 
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setup()
-    }
-    required public init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        setup()
-    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
 
-    private func setup() {
-        self.inactivePlaceholderTextColor = UBColor.descColor
-        self.inactiveLineColor = UIColor.init(hexString: "c3d0d4")
-        self.activeLineColor = UBColor.titleColor;
-        self.font = UIFont.systemFont(ofSize: 18)
-        self.textColor = UBColor.titleColor;
+        if self.firstInit {
+            self.firstInit = false
+            
+            self.inactivePlaceholderTextColor = UBColor.descColor
+            self.inactiveLineColor = UIColor.init(hexString: "c3d0d4")
+            self.activeLineColor = UBColor.titleColor;
+            self.font = UIFont.systemFont(ofSize: 18)
+            self.textColor = UBColor.titleColor;
+        }
     }
 }
