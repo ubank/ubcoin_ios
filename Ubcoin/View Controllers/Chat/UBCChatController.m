fbf6c109adebfdd8c1dff58ab7e3fb4ce8562c24
Alex Ostroushko <osapav@gmail.com>
2018-08-30 12:36:08 +0300

    fix chat closing

--- Ubcoin/View Controllers/Chat/UBCChatController.m
+++ Ubcoin/View Controllers/Chat/UBCChatController.m
@@ -103,10 +103,13 @@
 {
     if ([[UIApplication sharedApplication] canOpenURL:appURL])
     {
-        [self.navigationController popViewControllerAnimated:YES];
         [[UIApplication sharedApplication] openURL:appURL
                                            options:@{}
                                  completionHandler:nil];
+        
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [self.navigationController popViewControllerAnimated:NO];
+        });
     }
     else
     {
