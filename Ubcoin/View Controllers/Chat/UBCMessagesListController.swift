c671bca423b8153e049d4ae7a2798f50c32d988b
Alex Ostroushko <osapav@gmail.com>
2019-01-17 17:04:35 +0300

    move deals to profile

---
+++ Ubcoin/View Controllers/Chat/UBCMessagesListController.swift
@@ -0,0 +1,19 @@
+//
+//  UBCMessagesListController.swift
+//  Ubcoin
+//
+//  Created by Alex Ostroushko on 17/01/2019.
+//  Copyright © 2019 UBANK. All rights reserved.
+//
+
+import UIKit
+
+class UBCMessagesListController: UBViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "str_messages"
+    }
+
+}
