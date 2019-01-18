2c6a2fcfdb282ee617b41ae433317fb40ac53451
Alex Ostroushko <osapav@gmail.com>
2019-01-18 20:34:22 +0300

    setup messages list ui

--- Ubcoin/View Controllers/Chat/UBCMessagesListController.swift
+++ Ubcoin/View Controllers/Chat/UBCMessagesListController.swift
@@ -10,10 +10,42 @@ import UIKit
 
 class UBCMessagesListController: UBViewController {
 
+    private var content = [UBTableViewRowData]()
+    
+    private lazy var tableView: UBDefaultTableView = {
+        let tableView = UBDefaultTableView()
+        tableView.actionDelegate = self
+        
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.title = "str_messages"
+        
+        setupTableView()
+        updateInfo()
     }
 
+    private func setupTableView() {
+        view.addSubview(tableView)
+        view.addConstraints(toFillSubview: tableView)
+        
+        tableView.emptyView.icon.image = UIImage(named: "chat_empty_messages")
+        tableView.emptyView.title.text = "str_no_messages_title".localizedString()
+        tableView.emptyView.desc.text = "str_no_messages_desc".localizedString()
+        tableView.backgroundColor = UBColor.backgroundColor
+    }
+    
+    override func updateInfo() {
+        tableView.emptyView.isHidden = false
+    }
+}
+
+extension UBCMessagesListController: UBDefaultTableViewDelegate {
+    
+    func didSelect(_ data: UBTableViewRowData!, indexPath: IndexPath!) {
+        
+    }
 }
